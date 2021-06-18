b0ecbf3e1a642723b32193526ab1ceecdaac3915
Administrator <admin@example.com>
2021-06-18 07:10:59 +0430

    fix select box selected value

--- apps/mishka_html/lib/mishka_html_web/live/components/admin/form/editor_component.ex
+++ apps/mishka_html/lib/mishka_html_web/live/components/admin/form/editor_component.ex
@@ -1,4 +1,14 @@
 defmodule MishkaHtmlWeb.Admin.Form.EditorComponent do
   use MishkaHtmlWeb, :live_component
 
+  def render(assigns) do
+    ~L"""
+    <div id="editor-main-dive" class="col-sm-12 editor-diver" phx-update="ignore">
+        <div id="editor" phx-hook="Editor" class="bw" phx-update="ignore"></div>
+    </div>
+    <div class="form-error-tag" id="editor-tag-error">
+        <%= error_tag @f, String.to_atom(@form.type) %>
+    </div>
+    """
+  end
 end
--- apps/mishka_html/lib/mishka_html_web/live/components/admin/form/select_component.ex
+++ apps/mishka_html/lib/mishka_html_web/live/components/admin/form/select_component.ex
@@ -6,7 +6,11 @@ defmodule MishkaHtmlWeb.Admin.Form.SelectComponent do
       <div class="<%= @form.class %> field">
       <%= label @f , "#{MishkaHtmlWeb.AdminBlogCategoryLive.search_fields(@form.type).title}:" %>
       <button phx-click="delete_form" phx-value-type="<%= @form.type %>" type="button" class="btn-close" aria-label="Close"></button>
-      <%= select @f, String.to_atom(@form.type), MishkaHtmlWeb.AdminBlogCategoryLive.search_fields(@form.type).options, class: "form-select" %>
+      <%= select @f, String.to_atom(@form.type),
+        MishkaHtmlWeb.AdminBlogCategoryLive.search_fields(@form.type).options,
+        class: "form-select",
+        selected: "#{@form.value}"
+      %>
       <div class="form-error-tag">
           <%= error_tag @f, String.to_atom(@form.type) %>
       </div>
